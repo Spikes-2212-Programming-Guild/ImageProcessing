a2b70479516118386f0b18dce298efdda6eaf70d
TheArchNemesis <simon.kharmatsky@gmail.com>
2018-03-17 14:37:44 +0000

    reformated and fixed loop ending prints

--- nt_handler.py
+++ nt_handler.py
@@ -24,6 +24,7 @@ def init_nt():
     global nt
     nt = NetworkTables.getTable("ImageProcessing")
 
+
 def update_pipeline():
     global capturing
     global nt
@@ -31,20 +32,23 @@ def update_pipeline():
     global cam_id
     last_name = ""
     pipe_name = nt.getString("pipelineName", defaultValue="power-cube")
-    while capturing:
-        pipe_name = nt.getString("pipelineName", defaultValue="power-cube")
-	print "pipeline name is " + pipe_name
-        if pipe_name is not last_name:
-            if pipe_name == "ref":
-		print "switching to reflective"
-                pipeline = reflectives_pipeline.GripPipeline()
-                set_exposure_for_cameras(5, cam_id)
-            if pipe_name == "power-cube":
-		print "switching to power cube"
-                pipeline = powercube_pipeline.GripPipeline()
-                set_exposure_for_cameras(9, cam_id)
+    try:
+        while capturing:
+            pipe_name = nt.getString("pipelineName", defaultValue="power-cube")
+            print "pipeline name is " + pipe_name
+            if pipe_name is not last_name:
+                if pipe_name == "ref":
+                    print "switching to reflective"
+                    pipeline = reflectives_pipeline.GripPipeline()
+                    set_exposure_for_cameras(5, cam_id)
+                if pipe_name == "power-cube":
+                    print "switching to power cube"
+                    pipeline = powercube_pipeline.GripPipeline()
+                    set_exposure_for_cameras(9, cam_id)
 
-        last_name = pipe_name
+            last_name = pipe_name
+    finally:
+        print('Update pipeline is done')
 
 
 def update_image():
@@ -58,7 +62,7 @@ def update_image():
     try:
         while capturing:
             try:
-		#print "capturing image"
+                # print "capturing image"
                 cam_id = int(nt.getNumber("currentCamera", defaultValue=0))
                 if last_id != cam_id:
                     cam.release()
@@ -66,11 +70,11 @@ def update_image():
                 last_id = cam_id
                 success, im = cam.read()
             except Exception:
-		print "Exception"
+                print "Exception"
                 continue
     finally:
         cam.release()
-        print "Thread's done!"
+        print "Update Image Is Done"
 
 
 if __name__ == "__main__":
@@ -80,44 +84,44 @@ if __name__ == "__main__":
     init_nt()
     t_update_image = Thread(target=update_image)
     t_update_image.start()
-    #init_nt()
+    # init_nt()
     t_update_pipeline = Thread(target=update_pipeline)
     t_update_pipeline.start()
     contour_count = 2
     try:
         while im == None or not NetworkTables.isConnected():
-	     pass
-    #        print "NT connection: %r" % NetworkTables.isConnected()
+            pass
+            #        print "NT connection: %r" % NetworkTables.isConnected()
         [nt.delete(s) for s in nt.getKeys()]
-	print "starting processing"
+        print "starting processing"
         while True:
-            #print "Processing..."
+            # print "Processing..."
             if pipeline is not None:
-		print "has pipeline"
-		try:
-               		pipeline.process(im)
-                	contours = sorted(pipeline.filter_contours_output, key=cv2.contourArea, reverse=True)
-                	contour_count = max(contour_count, len(contours))
-                		#print "contours: ", len(contours)
-                	for i, c in enumerate(contours):
-                		nt.putNumber('contourArea%d' % i, cv2.contourArea(c))
-                    		print 'contourArea%d' % i, cv2.contourArea(c)
-                    		x, y, w, h = cv2.boundingRect(c)
-                    		nt.putNumber('width%d' % i, w)
-                    		# print 'width%d' % i, w
-                    		nt.putNumber('hight%d' % i, h)  # CR: typo lol
-                    		#	 print 'height%d' % i, h
-                    		nt.putNumber('x%d' % i, x)
-                    		# print 'x%d' % i, x
-                    		nt.putNumber('y%d' % i, y)
-                    		# print 'y%d' % i, y
-                    		nt.putBoolean('isUpdated%d' % i, True)
-                    		nt.putNumber('numberOfContours', len(contours))
-                    		print "isUpdated%d" % i, True
-                	for i in range(len(contours), contour_count):
-                    		nt.putBoolean('isUpdated%d' % i, False)
-		except Exception:
-			continue
+                print "has pipeline"
+                try:
+                    pipeline.process(im)
+                    contours = sorted(pipeline.filter_contours_output, key=cv2.contourArea, reverse=True)
+                    contour_count = max(contour_count, len(contours))
+                    # print "contours: ", len(contours)
+                    for i, c in enumerate(contours):
+                        nt.putNumber('contourArea%d' % i, cv2.contourArea(c))
+                        print 'contourArea%d' % i, cv2.contourArea(c)
+                        x, y, w, h = cv2.boundingRect(c)
+                        nt.putNumber('width%d' % i, w)
+                        # print 'width%d' % i, w
+                        nt.putNumber('hight%d' % i, h)  # CR: typo lol
+                        #	 print 'height%d' % i, h
+                        nt.putNumber('x%d' % i, x)
+                        # print 'x%d' % i, x
+                        nt.putNumber('y%d' % i, y)
+                        # print 'y%d' % i, y
+                        nt.putBoolean('isUpdated%d' % i, True)
+                        nt.putNumber('numberOfContours', len(contours))
+                        print "isUpdated%d" % i, True
+                    for i in range(len(contours), contour_count):
+                        nt.putBoolean('isUpdated%d' % i, False)
+                except Exception:
+                    continue
     finally:
         capturing = False
         print "Job's done!"
